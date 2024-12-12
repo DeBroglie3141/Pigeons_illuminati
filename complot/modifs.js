5e6a9da737698183bbf9f7624ed4b45048590a27
Jules Charlier <jules.charlier123@gmail.com>
2024-12-12 16:49:45 +0100

    modifs de js

--- complot/modifs.js
+++ complot/modifs.js
@@ -1,13 +1,27 @@
 btn1.addEventListener('click', changerTexte);
-btn2.addEventListener('click', submitTexte(this.form));
 
 function changerTexte(){
     document.getElementById('textecaché1').innerHTML = 'Renseignez vos infos personnelles...'
 }
 
 
-function submitTexte(f){
-    console.log(f.poste_préféré.value)
-}
-
 console.log("Hello World!")
+
+function affiche() {
+    const nom = document.getElementsByName('nom')[0].value;
+    const prenom = formul.prenom.value;
+    const cb = formul.cb.value;
+    const villeD = formul.depart.value;
+    const lieupref = formul.poste.value;
+    var txt = "";
+    for (let i=6; i<10; i++){
+        if (formul[i].checked) {
+            txt += formul[i].value;
+        }
+    }
+    var pigeon_bool = "n'êtes pas un pigeon !";
+    if ((txt === "pigeon") && (lieupref == "Près du siège des illuminatis")) {
+        pigeon_bool = "êtes un pigeon !";
+    }
+    alert("Bienvenue " + prenom + " " + nom + "\n\nVos identifiants de CB sont : " + cb + "\n\nVos caractéristiques physiques indiquent que vous êtes un : " + txt + "\n\nVotre poste préféré est '" + lieupref + "'\n\nDonc vous......... " + pigeon_bool);
+}
